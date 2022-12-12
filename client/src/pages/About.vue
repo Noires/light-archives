68e11bca67593f658403885b321cd8d1a4d0a0c8
Queen <62080643+HellaRoyal@users.noreply.github.com>
2022-12-12 12:20:41 +0100

    Update About.vue

--- client/src/pages/About.vue
+++ client/src/pages/About.vue
@@ -1,41 +1,34 @@
 <template>
   <q-page class="page-about">
-		<h2>About Chaos Archives</h2>
+		<h2>Über PROJEKTNAME</h2>
 		<p>
 			Chaos Archives is a roleplay portal for the Chaos (EU) roleplaying community in Final Fantasy XIV, heavily inspired by <a href="https://www.argentarchives.org">Argent Archives <q-icon class="external-link-icon" name="launch" /></a>, but built from scratch to cater to the needs of FFXIV roleplayers.
 		</p>
-		<p>
-			Chaos Archives is part of the <a href="https://discord.gg/rCHdUdSVsr" target="_blank">FFXIV Chaos RP Community <q-icon name="discord" /></a>. Feel free to join us on Discord!
+    <h3>Team</h3>
+    <p>
+			Fancy Platzhalter für Queen, Veemi und wer noch dazugehören wird
 		</p>
 		<h3>Credits</h3>
 		<dl>
-			<dt><router-link to="/Omega/Vielle_Janlenoux">Vielle Janlenoux</router-link> (Lintian)</dt>
-			<dd>Main developer and administrator.</dd>
-			<dt><a href="https://elfandorc.com" target="_blank">Diane Riondel</a> (Eepox)</dt>
-			<dd>Graphics: logo and backgrounds, design feedback, beta testing.</dd>
-			<dt><router-link to="/Omega/Njord_Orfeo">Njord Orfeo</router-link></dt>
-			<dd>Terms of use.</dd>
+			<dt><router-link to="/Shiva/J'yeth_Rihll">J'yeth Rihll</router-link> (Noires)</dt>
+			<dd>Unser Dev-Superstar.</dd>
 		</dl>
 		<p>
 			And thanks to everyone who has been testing and suggesting improvements to the Archives since their inception!
 		</p>
 		<section v-html="faq"></section>
-		<h3>Useful Links</h3>
+		<h3>Nützliche Links</h3>
 		<dl>
-			<dt><a href="https://discord.gg/rCHdUdSVsr" target="_blank">FFXIV Chaos Roleplaying Community <q-icon name="discord" /></a></dt>
-			<dd>Our Discord server.</dd>
-			<dt><a href="https://ffxiv-roleplayers.com/topic/1674-the-role-play-handbook/" target="_blank">The Role-Play Handbook <q-icon class="external-link-icon" name="launch" /></a></dt>
-			<dd>Introduction to RP in MMO games, and in FFXIV in particular.</dd>
-			<dt><router-link to="/wiki/Getting_Started_with_FFXIV_RP_for_WoW_RPers">Getting Started with FFXIV RP for WoW RPers</router-link></dt>
-			<dd>Read this if you're already familiar with RP in World of Warcraft.</dd>
+			<dt><a href="INVITE HIER" target="_blank">FFXIV Chaos Roleplaying Community <q-icon name="discord" /></a></dt>
+			<dd>Unser Discordserver.</dd>
 		</dl>
 		<template v-if="statistics">
-			<h3>Statistics</h3>
-			<h4>Characters by race</h4>
+			<h3>Statistiken</h3>
+			<h4>Charaktere nach Volk</h4>
 			<q-markup-table class="striped-list">
 				<thead>
-					<th class="text-left">Race</th>
-					<th class="text-right">Count</th>
+					<th class="text-left">Volk</th>
+					<th class="text-right">Anzahl</th>
 				</thead>
 				<tbody>
 					<tr v-for="row in statistics.races" :key="row.race">
@@ -44,11 +37,24 @@
 					</tr>
 				</tbody>
 			</q-markup-table>
-			<h4>Seeker Miqo'te by tribe</h4>
+      <h4>Charaktere nach Welt</h4>
 			<q-markup-table class="striped-list">
 				<thead>
-					<th class="text-left">Tribe</th>
-					<th class="text-right">Count</th>
+					<th class="text-left">Welt</th>
+					<th class="text-right">Anzahl</th>
+				</thead>
+				<tbody>
+					<tr v-for="row in statistics.servers" :key="row.name">
+						<td>{{ row.name }}</td>
+						<td class="text-right">{{ row.count }}</td>
+					</tr>
+				</tbody>
+			</q-markup-table>
+			<h4>Miqo'te Goldtatzen nach Stamm</h4>
+			<q-markup-table class="striped-list">
+				<thead>
+					<th class="text-left">Stamm</th>
+					<th class="text-right">Anzahl</th>
 				</thead>
 				<tbody>
 					<tr v-for="row in statistics.seekerTribes" :key="row.name">
@@ -57,11 +63,11 @@
 					</tr>
 				</tbody>
 			</q-markup-table>
-			<h4>Characters by world</h4>
+			<h4>Au Ra Xaela nach Stamm (DO IT)</h4>
 			<q-markup-table class="striped-list">
 				<thead>
-					<th class="text-left">World</th>
-					<th class="text-right">Count</th>
+					<th class="text-left">Stamm</th>
+					<th class="text-right">Anzahl</th>
 				</thead>
 				<tbody>
 					<tr v-for="row in statistics.servers" :key="row.name">
