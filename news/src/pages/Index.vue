b8b748d664859ffe041e36329081d2da3396aa21
Queen <62080643+HellaRoyal@users.noreply.github.com>
2022-12-15 16:22:11 +0100

    Update Index.vue

--- news/src/pages/Index.vue
+++ news/src/pages/Index.vue
@@ -5,7 +5,7 @@
       <article-view v-for="article in issue.articles" :article="article" :key="article.id" />
     </template>
     <template v-else>
-      Es gibt noch keine neuen Ausgaben.
+      Es gibt noch keine Ausgaben.
     </template>
   </section>
 </template>
