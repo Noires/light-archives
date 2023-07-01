ef8e05f94876192ac987d91464c2680e3aef2333
Dustin Baron <dustinbaron@online.de>
2023-07-01 17:08:51 +0200

    Add basic frontend for violations

---
+++ client/src/pages/Violations.vue
@@ -0,0 +1,43 @@
+<template>
+    <q-page class="page-violations">
+          <h2>Verstöße</h2>
+          <violations-list :violations="violations" />
+      </q-page>	
+</template>
+
+<script lang="ts">
+import { ViolationSummaryDto } from '@app/shared/dto/violations/violation-summary.dto';
+import { useApi } from 'src/boot/axios';
+import { notifyError } from 'src/common/notify';
+import VenueList from 'src/components/venues/VenueList.vue';
+import { Options, Vue } from 'vue-class-component';
+
+const $api = useApi();
+
+@Options({
+	name: 'PageVenues',
+	components: {
+		VenueList
+	},
+  async beforeRouteEnter(_, __, next) {
+    try {
+      const violations = await $api.violations.getViolations();
+      next(vm => (vm as PageViolations).setContent(violations));
+    } catch (e) {
+      console.log(e);
+      notifyError(e);
+    }
+  }
+})
+export default class PageViolations extends Vue {
+	violations: ViolationSummaryDto[] = [];
+
+	setContent(violations: ViolationSummaryDto[]) {
+		this.violations = violations;
+	}
+}
+</script>
+
+<style lang="scss">
+
+</style>
