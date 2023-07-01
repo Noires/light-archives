b68210eb328b5b895faab88df5088435deb4df86
Dustin Baron <dustinbaron@online.de>
2023-07-01 15:42:05 +0200

    Initial backend for violations display

--- server/apps/chaosarchives/src/api/internal/violations/violations.controller.ts
+++ server/apps/chaosarchives/src/api/internal/violations/violations.controller.ts
@@ -3,16 +3,28 @@ import { RoleRequired } from '@app/auth/decorators/role-required.decorator';
 import { UserInfo } from '@app/auth/model/user-info';
 import { ViolationReportDto } from '@app/shared/dto/violations/violation-report.dto';
 import { Role } from '@app/shared/enums/role.enum';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { ViolationsService } from './violations.service';
 
 @Controller('violations')
 export class ViolationsController {
 	constructor(private violationsService: ViolationsService) {}
 
+	@Get()
+	@RoleRequired(Role.ADMIN)
+	async getViolations(): Promise<ViolationReportDto[]> {
+		return this.violationsService.getViolations();
+	}
+
 	@Post()
 	@RoleRequired(Role.USER)
 	async reportViolation(@Body() report: ViolationReportDto, @CurrentUser() user: UserInfo): Promise<void> {
 		await this.violationsService.reportViolation(report, user);
+	}
+
+	@Delete(':id')
+	@RoleRequired(Role.ADMIN)
+	async deleteStory(@Param('id', ParseIntPipe) id: number): Promise<void> {
+		await this.violationsService.deleteViolation(id);
 	}
 }
